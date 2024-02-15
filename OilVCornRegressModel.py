6926a5d9ce2da4f3dddf0e2ab67696345ba22783
jhhansen1 <143114076+jhhansen1@users.noreply.github.com>
2024-02-15 11:28:47 -0600

    Update OilVCornRegressModel.py

--- OilVCornRegressModel.py
+++ OilVCornRegressModel.py
@@ -1,1 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
+data1 = pd.read_csv("2._Energy_Prices.csv")
+x = data[[]]

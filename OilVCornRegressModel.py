d977964e1324096e9f1f04c8170ddcd860e7dd33
jhhansen1 <143114076+jhhansen1@users.noreply.github.com>
2024-02-16 11:10:22 -0600

    Update OilVCornRegressModel.py

--- OilVCornRegressModel.py
+++ OilVCornRegressModel.py
@@ -4,5 +4,21 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
-data1 = pd.read_csv("2._Energy_Prices.csv")
-x = data[[]]
+data = pd.read_csv("2._Energy_Prices.csv")
+y_1 = data["oil price"]
+y_2 = data["corn price"]
+x = data[time]
+# x_data=StandardScaler().fit_transform(x)
+# y_data=StandardScaler().fit_transform(y)
+fig, graph = plt.subplots(2)
+graph[0].scatter(x, y_1)
+graph[0].set_xlabel("Time")
+graph[0].set_ylabel("oil price")
+
+graph[1].scatter(x, y_2)
+graph[1].set_xlabel("Time")
+graph[1].set_ylabel("corn price")
+
+plt.tight_layout()
+plt.show()
+

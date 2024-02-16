ac602719e87b8014bcadc4542d92c6a37992f4d2
jhhansen1 <143114076+jhhansen1@users.noreply.github.com>
2024-02-16 11:23:50 -0600

    Update OilVCornRegressModel.py

--- OilVCornRegressModel.py
+++ OilVCornRegressModel.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
-data = pd.read_csv("2._Energy_Prices.csv")
+data = pd.read_csv("TimeVOilVCorn.csv")
 y_1 = data["oil price"]
 y_2 = data["corn price"]
 x = data["time"]

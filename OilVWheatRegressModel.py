52fd4d920b75846b5a047f930b4ae09a9f59d18e
alacatus <alacatus@cps.edu>
2024-02-23 10:58:12 -0600

    done

--- OilVWheatRegressModel.py
+++ OilVWheatRegressModel.py
@@ -2,36 +2,23 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
-data = pd.read_csv("2._Energy_Prices.csv")
+data = pd.read_csv("wheat-prices-historical-chart-data.csv")
+y_1 = data["oil price"]
+y_2 = data["wheat price"]
+x = data["Time"]
+# x_data=StandardScaler().fit_transform(x)
+# y_data=StandardScaler().fit_transform(y)
+fig, graph = plt.subplots(2)
+graph[0].scatter(x, y_1)
+graph[0].set_xlabel("Time")
+graph[0].set_ylabel("wheat price")
 
-x = data["Date"].values
-y = data["Price"].values
+graph[1].scatter(x, y_2)
+graph[1].set_xlabel("Time")
+graph[1].set_ylabel("wheat price")
 
-x = x.reshape(-1, 1)
-
-model = LinearRegression().fit(x,y)
-
-coef = round(float(model.coef_), 2)
-intercept = round(float(model.intercept_), 2)
-r_squared = model.score(x, y)
-
-x_predict = 2012024
-
-prediction = model.predict([[x_predict]])
-print(f"Model's Linear Equation: y = {coef}x + {intercept}")
-print(f"R Squared value: {r_squared}")
-print(f"Prediction when x is {x_predict}: {prediction}")
-
-plt.figure(figsize=(6,4))
-plt.scatter(x,y, c ="purple")
-plt.scatter(x_predict, prediction, c="blue")
-
-plt.xlabel("Dates (Monthly)")
-plt.ylabel("Prices of Corn Futures")
-plt.title("Monthly Prices of Corn Futures")
-plt.xticks(np.arange(2014, 2025, step=1))
-plt.plot(x, coef*x + intercept, c="r", label="Line of Best Fit")
-
-plt.legend
+plt.tight_layout()
 plt.show()
+

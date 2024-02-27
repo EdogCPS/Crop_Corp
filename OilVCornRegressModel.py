a4f9666d57c084dbeb11476f3f154ae7df2bcaca
Jacob <jhtruxal@cps.edu>
2024-02-27 11:24:44 -0600

    OilVTime

--- OilVCornRegressModel.py
+++ OilVCornRegressModel.py
@@ -5,20 +5,12 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
 data = pd.read_csv("TimeVOilVCorn.csv")
-y_1 = data["oil price"]
-y_2 = data["corn price"]
-x = data["Time"]
-# x_data=StandardScaler().fit_transform(x)
-# y_data=StandardScaler().fit_transform(y)
-fig, graph = plt.subplots(2)
-graph[0].scatter(x, y_1)
-graph[0].set_xlabel("Time")
-graph[0].set_ylabel("oil price")
-
-graph[1].scatter(x, y_2)
-graph[1].set_xlabel("Time")
-graph[1].set_ylabel("corn price")
-
+y = data["oil price"]
+x = data["monthsFrom2014"]
+fig, graph = plt.subplots(1)
+graph.scatter(x, y)
+graph.set_xlabel("Months from 2014")
+graph.set_ylabel("oil price")
 plt.tight_layout()
 plt.show()
 

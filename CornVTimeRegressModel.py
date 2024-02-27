6a811c0d3e787267f688284804bcbb320d3de1e4
Jacob <jhtruxal@cps.edu>
2024-02-27 11:31:59 -0600

    did stuff

--- CornVTimeRegressModel.py
+++ CornVTimeRegressModel.py
@@ -1,14 +1,18 @@
 import pandas as pd
 import numpy as np
-from sklearn import linear_model
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 
 data = pd.read_csv("TimeVOilVCorn.csv")
 y = data["corn price"]
-x_1 = data["time(year)"]
-x_2 = data["time(month)"]
+x = data["monthsFrom2014"]
+fig, graph = plt.subplots(1)
+graph.scatter(x, y)
+graph.set_xlabel("months from 2014")
+graph.set_ylabel("corn price")
+plt.tight_layout()
+plt.show()
 xtrain, xtest, ytrain, ytest= train_test_split(x, y, test_size=.2)
 log_reg=linear_model.LogisticRegression()
 log_reg.fit(xtrain,ytrain)

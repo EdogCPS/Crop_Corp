ab46e77d4c4ef88ef38189f75adf2713abe300ca
Jacob <jhtruxal@cps.edu>
2024-02-29 11:11:17 -0600

    I DID IT!!!!!!:);)

--- CornVTimeRegressModel.py
+++ CornVTimeRegressModel.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
+import sklearn.linear_model
 from sklearn.model_selection import train_test_split
 
 data = pd.read_csv("TimeVOilVCorn.csv")

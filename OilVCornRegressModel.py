07ec194d1fefa3790fb68e1aa800b11a61091408
Jacob <jhtruxal@cps.edu>
2024-02-29 11:30:35 -0600

    I DID IT AGAIN :)!!!!!!!!!

--- OilVCornRegressModel.py
+++ OilVCornRegressModel.py
@@ -5,20 +5,22 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
 data = pd.read_csv("TimeVOilVCorn.csv")
-y_1 = data["oil price"]
-y_2 = data["corn price"]
-x = data["Time"]
-# x_data=StandardScaler().fit_transform(x)
-# y_data=StandardScaler().fit_transform(y)
-fig, graph = plt.subplots(2)
-graph[0].scatter(x, y_1)
-graph[0].set_xlabel("Time")
-graph[0].set_ylabel("oil price")
-
-graph[1].scatter(x, y_2)
-graph[1].set_xlabel("Time")
-graph[1].set_ylabel("corn price")
-
+x = data["oil price"].values
+y = data["corn price"].values
+x = x.reshape(-1, 1)
+model = LinearRegression().fit(x,y)
+fig, graph = plt.subplots(1)
+graph.scatter(x, y)
+coef = round(float(model.coef_), 2)
+intercept = round(float(model.intercept_), 2)
+r_squared = model.score(x, y)
+x_predict=83.2    
+graph.set_xlabel("corn price")
+graph.set_xlabel("oil price")
+prediction = model.predict([[x_predict]])
+plt.plot(x, coef*x + intercept, c="r", label="Line of Best Fit")
+plt.legend
 plt.tight_layout()
-plt.show()
+print(f"Corn price prediction when oil is ${x_predict}: ${prediction}")
+plt.show
 
